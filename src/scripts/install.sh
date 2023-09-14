195a9f96db6c5ea803fbadac2833639d4f9f0d55
Kevin Sylvestre <kevin@ksylvest.com>
2023-09-14 11:50:13 -0700

    Ensure bun is available once installed

--- src/scripts/install.sh
+++ src/scripts/install.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
+# shellcheck disable=SC2016
 
 curl -fsSL https://bun.sh/install | bash
-export BUN_INSTALL="$HOME/.bun" >> "$BASH_ENV"
-export PATH="$BUN_INSTALL/bin:$PATH" >> "$BASH_ENV"
-bun --version
+echo 'export BUN_INSTALL="$HOME/.bun"' >> "$BASH_ENV"
+echo 'export PATH="$BUN_INSTALL/bin:$PATH"' >> "$BASH_ENV"
